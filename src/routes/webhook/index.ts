2089ff0d9518e29de52121935c08c96dacddbe52
Alessandro Mesti <mesti.alessandro.git@gmail.com>
2026-01-07 22:14:49 +0100

    Added one space

--- src/routes/webhook/index.ts
+++ src/routes/webhook/index.ts
@@ -4,6 +4,7 @@ const webhook: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get('/whatsapp', async function (request, reply) {
     return 'this is an example'
   })
+
   fastify.get('/telegram', async function (request, reply) {
     return 'this is an example'
   })
