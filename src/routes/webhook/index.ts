f830d3602118619146be7580bf757fbe81b1f0d0
Trick-o-Trade <trickotrade@gmail.com>
2026-01-07 16:51:52 +0100

    Starting templated project with fastify to create webhook fir incoming messages from 3rd party services

---
+++ src/routes/webhook/index.ts
@@ -0,0 +1,12 @@
+import { FastifyPluginAsync } from 'fastify'
+
+const webhook: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
+  fastify.get('/whatsapp', async function (request, reply) {
+    return 'this is an example'
+  })
+  fastify.get('/telegram', async function (request, reply) {
+    return 'this is an example'
+  })
+}
+
+export default webhook
